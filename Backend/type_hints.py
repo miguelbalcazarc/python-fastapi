32a329480bdc5f6bf721f389ff55fb3560de46f4
Miguel Balcazar <miguel.balcazar@externos.bci.cl>
2023-02-22 23:43:28 -0500

    se agrega main

---
+++ Backend/type_hints.py
@@ -0,0 +1,20 @@
+### Type Hints ###
+
+#Trabajar obligatoriamente con el tipo de dato al momento de declarar una variable
+
+my_string_variable = "My string variable"
+
+print(my_string_variable)
+print(type(my_string_variable))
+
+my_string_variable = 5
+
+print(my_string_variable)
+print(type(my_string_variable))
+
+my_typed_variable: str = "My typed String variable"
+print(my_typed_variable)
+
+my_typed_variable = 5
+print(my_typed_variable)
+print(type(my_typed_variable))
